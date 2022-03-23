cb374f0d4aeaa2ecca61595054adb7fd6bb8c422
jh.lee <tyrfingr12@naver.com>
2022-03-23 17:02:22 +0900

    update

--- src/server.js
+++ src/server.js
@@ -17,13 +17,15 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
 
 wss.on("connection",(socket)=>{
+    sockets.push(socket);
     console.log("Connected to Browser");
     socket.on("close", () => console.log("connetion closed from server"));
-    socket.send("hello!!");
     socket.on("message",(message) => {
-        console.log(message.toString('utf8'));
+        socket.send(message.toString('utf8'));
     })
+    socket.send("hello!!");
 })
 server.listen(3000, handleListen);
